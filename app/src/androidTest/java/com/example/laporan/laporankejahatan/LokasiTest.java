6f911174e6b4459e77568396994776b052065717
widyaSaputra <widyasaputra150@gmail.com>
2017-10-24 16:10:01 +0700

    Update LokasiTest.java

--- app/src/androidTest/java/com/example/laporan/laporankejahatan/LokasiTest.java
+++ app/src/androidTest/java/com/example/laporan/laporankejahatan/LokasiTest.java
@@ -1,14 +1,34 @@
 package com.example.laporan.laporankejahatan;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.test.espresso.intent.Intents;
+import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -22,17 +42,24 @@ import static org.junit.Assert.*;
 /**
  * Created by HP on 18/10/2017.
  */
-public class LokasiTest {
+public class LokasiTest extends AppCompatActivity {
 
     @Rule
     public ActivityTestRule<Lokasi> lokasiActivityTestRule = new ActivityTestRule<Lokasi>(Lokasi.class);
     private Lokasi loka = null;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference reee = database.getReference("Laporan");
+    LocationManager locationManager;
+
+    long a, b = 0;
 
     @Before
     public void setUp() throws Exception {
         Intents.init();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         loka = lokasiActivityTestRule.getActivity();
         FirebaseAuth.getInstance().signInWithEmailAndPassword("magrid@gmail.com", "123456");
+
     }
 
     @After
@@ -51,10 +78,23 @@ public class LokasiTest {
     }
 
     @Test
-    public void testLokasi(){
-        onView(withId(R.id.buttonSend)).perform(click());
+    public void testLokasi() {
         pause(2000);
-        //intended(hasComponent(Maps.class.getName()));
-    }
+        reee.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                FirebaseAuth firebaseAuth;
+                firebaseAuth = FirebaseAuth.getInstance();
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                b = dataSnapshot.child(user.getUid()).child("Statistik").getChildrenCount();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        onView(withId(R.id.buttonSend)).perform(click());
+        assertEquals(16, b);
+    }
 }
